f09879e43d3de8442be2aa7409eff25c6b28d95b
1rodriguezfranco <98francorodriguez@gmail.com>
2023-07-11 22:14:46 -0300

    Vinculación de vistas con controladores para creación, edición y eliminación de mascotas, categorías y marcas

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,5 +1,29 @@
+const path = require('path');
+const db = require(path.join(__dirname, "../../database/models"));
+
 const adminController = {
-    table: (req, res) =>{ res.render("./admin/table") }
+    table: (req, res) =>{ res.render("./admin/table") },
+    createPet: async (req, res) => {
+		return res.render("./admin/pets/createPet.ejs");
+    },
+    updateDeletePet: async (req, res) => {
+		let pets = await db.Pet.findAll();
+		return res.render("./admin/pets/updateDeletePet.ejs", {pets});
+    },
+    createCategory: async (req, res) => {
+		return res.render("./admin/categories/createCategory.ejs");
+    },
+    updateDeleteCategory: async (req, res) => {
+		let categories = await db.ProductCategory.findAll();
+		return res.render("./admin/categories/updateDeleteCategory.ejs", {categories});
+    },
+    createBrand: async (req, res) => {
+		return res.render("./admin/brands/createBrand.ejs");
+    },
+    updateDeleteBrand: async (req, res) => {
+		let brands = await db.Brand.findAll();
+		return res.render("./admin/brands/updateDeleteBrand.ejs", {brands});
+    }
 };
 
 module.exports = adminController;
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,4 +5,14 @@ const adminController = require("../controllers/adminController");
 
 router.get("/table", adminController.table);
 
+router.get("/create/pet", adminController.createPet);
+router.get("/update-delete/pet", adminController.updateDeletePet);
+
+router.get("/create/category", adminController.createCategory);
+router.get("/update-delete/category", adminController.updateDeleteCategory);
+
+router.get("/create/brand", adminController.createBrand);
+router.get("/update-delete/brand", adminController.updateDeleteBrand);
+
+
 module.exports = router;
