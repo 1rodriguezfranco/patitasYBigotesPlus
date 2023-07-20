32f153a575a3cfd69c162e23503265639a54cfa0
1rodriguezfranco <98francorodriguez@gmail.com>
2023-07-19 21:50:56 -0300

    Otorgándole estilos y formato responsive a vistas createPBC y updatePBC

--- src/middlewares/validations/admin/validationEditPBC.js
+++
@@ -1,7 +0,0 @@
-const { body } = require('express-validator');
-
-const validations = [
-    body('name').notEmpty().withMessage('El nombre es obligatorio')
-];
-
-module.exports = validations;
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,27 +2,26 @@ const express = require("express");
 const path = require('path');
 const router = express.Router();
 const adminController = require("../controllers/adminController");
-const validationsCreatePBC = require(path.join(__dirname, '../middlewares/validations/admin/validationCreatePBC.js'));
-const validationsEditPBC = require(path.join(__dirname, '../middlewares/validations/admin/validationEditPBC.js'));
+const validationsPBC = require(path.join(__dirname, '../middlewares/validations/admin/validationPBC.js'));
 
 router.get("/table", adminController.table);
 
 router.get("/create/pet", adminController.createPet);
-router.post("/create/pet", validationsCreatePBC, adminController.storePet);
+router.post("/create/pet", validationsPBC, adminController.storePet);
 router.get("/update/pet/:id", adminController.editPet);
-router.put("/update/pet/:id", validationsEditPBC, adminController.updatePet);
+router.put("/update/pet/:id", validationsPBC, adminController.updatePet);
 router.delete("/delete/pet/:id", adminController.deletePet);
 
 router.get("/create/category", adminController.createCategory);
-router.post("/create/category", validationsCreatePBC, adminController.storeCategory);
+router.post("/create/category", validationsPBC, adminController.storeCategory);
 router.get("/update/category/:id", adminController.editCategory);
-router.put("/update/category/:id", adminController.updateCategory);
+router.put("/update/category/:id", validationsPBC, adminController.updateCategory);
 router.delete("/delete/category/:id", adminController.deleteCategory);
 
 router.get("/create/brand", adminController.createBrand);
-router.post("/create/brand", validationsCreatePBC, adminController.storeBrand);
+router.post("/create/brand", validationsPBC, adminController.storeBrand);
 router.get("/update/brand/:id", adminController.editBrand);
-router.put("/update/brand/:id", adminController.updateBrand);
+router.put("/update/brand/:id", validationsPBC, adminController.updateBrand);
 router.delete("/delete/brand/:id", adminController.deleteBrand);
 
 router.get("/users", adminController.manageUsers);
